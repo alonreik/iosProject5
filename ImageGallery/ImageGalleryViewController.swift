02d6dbcb5175519127ce5a44a8813f5f90f18a74
alonreik <reik@cherrypick-consulting.com>
2021-04-07 17:37:47 +0300

    Only defined a collection view and pasted some images' URL.

---
+++ ImageGallery/ImageGalleryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Alon Reik on 07/04/2021.
+//
+
+import UIKit
+
+class ImageGalleryViewController: UICollectionViewController {
+
+    // urls of images.
+    let imagesURLs = [
+        "https://media.bleacherreport.com/f_auto,w_800,h_533,q_auto,c_fill/br-img-images/003/872/788/hi-res-2c4869a446d305ffae628b510cb6131f_crop_north.jpg",
+        "https://static01.nyt.com/images/2020/06/12/sports/12nba-return/merlin_168451203_493eb598-93f6-47dc-9140-c8bd94b620da-superJumbo.jpg?quality=90&auto=webp",
+        "https://swordstoday.ie/wp-content/uploads/2021/03/getobject-47-e1575408347332-770x462.jpeg"
+    ]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+     
+
+
+}
+
