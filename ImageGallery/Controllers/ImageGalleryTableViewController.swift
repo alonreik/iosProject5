035fff8a2f1266e37da0b08e4d4a47f197bd4249
Alon <reik@cherrypick-consulting.com>
2021-10-27 12:29:43 +0300

    performed some code formatting so it will look better

--- ImageGallery/Controllers/ImageGalleryTableViewController.swift
+++ ImageGallery/Controllers/ImageGalleryTableViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ImageGalleryTableViewController.swift
-//  ImageGallery
-//
-//  Created by Alon Reik on 12/04/2021.
-//
-
 import UIKit
 
 // todo - what do we do when we try to fetch an image but fail?
@@ -14,36 +7,58 @@ class ImageGalleryTableViewController: UITableViewController {
     // the model (for this MVC):
     var galleryNames = ["NBA", "Euroleague"]
     var deletedGalleries: [String] = []
-    
     var moreGalleryName = ["G League", "La Liga" ,"French League", "Winner League"]
     
-    
     private let URLs = ["https://media.bleacherreport.com/f_auto,w_800,h_533,q_auto,c_fill/br-img-images/003/872/788/hi-res-2c4869a446d305ffae628b510cb6131f_crop_north.jpg", "https://static01.nyt.com/images/2020/06/12/sports/12nba-return/merlin_168451203_493eb598-93f6-47dc-9140-c8bd94b620da-superJumbo.jpg?quality=90&auto=webp", "https://swordstoday.ie/wp-content/uploads/2021/03/getobject-47-e1575408347332-770x462.jpeg",
-    "https://static.timesofisrael.com/www/uploads/2019/07/AP_18337090034812-e1563112655347.jpg",
-    "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/996.png&w=350&h=254",
-    "https://basket.co.il/pics/2019/2020/14ob21.jpg",
-    "https://external-preview.redd.it/WRyCAB2n7HQQURRTNIc6OkC58swUT1CxbIYhBh900DQ.jpg?auto=webp&s=57f10a7cbba40c7758d9b7e1576a3ce98122d623"
+        "https://static.timesofisrael.com/www/uploads/2019/07/AP_18337090034812-e1563112655347.jpg",
+        "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/996.png&w=350&h=254",
+        "https://basket.co.il/pics/2019/2020/14ob21.jpg",
+        "https://external-preview.redd.it/WRyCAB2n7HQQURRTNIc6OkC58swUT1CxbIYhBh900DQ.jpg?auto=webp&s=57f10a7cbba40c7758d9b7e1576a3ce98122d623"
     ]
     
     lazy private var models = [
-        // array of image instances (images of NBA players)
         "NBA": [ImageGalleryItem(url: self.URLs[0]), ImageGalleryItem(url: self.URLs[1]), ImageGalleryItem(url: self.URLs[2]), ImageGalleryItem(url: self.URLs[6])],
-        // array of image instances (images of euroleague players)
         "Euroleague": [ImageGalleryItem(url: self.URLs[3]), ImageGalleryItem(url: self.URLs[4]),ImageGalleryItem(url: self.URLs[5])]
     ]
+
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let cell = sender as? ImageGalleryTableViewCell, let rowName = cell.textLabel?.text, let destinationGallery = models[rowName] {
+            if segue.identifier == "Choose Gallery" {
+                if let destination = segue.destination as? ImageGalleryCollectionViewController {
+                    // set the image gallery to be the appropiate array of Image instances.
+                    destination.imageGallery = destinationGallery
+                    // set the title of the segue.destination to be name of the picked gallery.
+                    destination.title = rowName
+                }
+            }
+        }
     }
-
-    // MARK: - Table view data source
-
-    // How many sections in the tableview?
+    
+    /* -------------------
+     Functions for Buttons
+     ---------------------*/
+    
+    @IBAction func addGallery(_ sender: Any) {
+        guard let galleryName = self.moreGalleryName.popLast() else {
+            print("out of gallery names")
+            return
+        }
+        galleryNames.append(galleryName)
+        tableView.performBatchUpdates({
+            if (galleryNames.count == 1){
+                tableView.insertSections([0], with: .automatic)
+            }
+            tableView.insertRows(at: [IndexPath(row: galleryNames.count - 1, section: 0)], with: .automatic)
+        }, completion: nil)
+    }
+    
+    
+    /* -------------------
+     Tableview data source methods
+     ---------------------*/
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // There are 2 sections: one for the gallery names, and one for the deleted galleries names.
         // If no gallery is currently in the deleted galleries section, then only 1 section is displayed.
@@ -56,7 +71,6 @@ class ImageGalleryTableViewController: UITableViewController {
         }
     }
     
-    // How many rows per section in the tableview?
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if galleryNames.count > 0 && section == 0 {
             return self.galleryNames.count
@@ -65,7 +79,6 @@ class ImageGalleryTableViewController: UITableViewController {
         }
     }
 
-    // What do display in each row\cell of the table view?
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "GalleryCell", for: indexPath)
         if let galleryTableCell = cell as? ImageGalleryTableViewCell {
@@ -76,7 +89,6 @@ class ImageGalleryTableViewController: UITableViewController {
         }
     }
     
-    // What happens when somebody selects a row? preforms segue.
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // Pressing a tableView row will segue to the gallery only if the gallery-name is not in the deleted section.
         if galleryNames.count > 0 && indexPath.section == 0 {
@@ -84,7 +96,6 @@ class ImageGalleryTableViewController: UITableViewController {
         }
     }
     
-    //
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if galleryNames.count > 0 && section == 0 {
             return "Gallery Names"
@@ -92,15 +103,7 @@ class ImageGalleryTableViewController: UITableViewController {
             return "Recently Deleted"
         }
     }
-
-//    // Override to support conditional editing of the table view.
-//    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//        // Return false if you do not want the specified item to be editable.
-//        return true
-//    }
     
-    
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             if galleryNames.count > 0 && indexPath.section == 0 {
@@ -111,13 +114,11 @@ class ImageGalleryTableViewController: UITableViewController {
                     if (deletedGalleries.count == 1) {
                         // If we need to create the second section:
                         tableView.insertSections([1], with: .automatic)
-                        tableView.deleteRows(at: [indexPath], with: .automatic)
                     } else if (galleryNames.isEmpty) {
                         // If we need to delete the first section
                         tableView.deleteSections([0], with: .automatic)
-                    } else {
-                        tableView.deleteRows(at: [indexPath], with: .automatic)
                     }
+                    tableView.deleteRows(at: [indexPath], with: .automatic)
                     tableView.insertRows(at: [IndexPath(row: deletedGalleries.count - 1, section: galleryNames.isEmpty ? 0 : 1)], with: .automatic)
                 }, completion: nil)
             } else {
@@ -129,26 +130,8 @@ class ImageGalleryTableViewController: UITableViewController {
                     }
                 }, completion: nil)
             }
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
-    
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
     override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         if (galleryNames.count > 0 && indexPath.section == 0) {
@@ -163,7 +146,7 @@ class ImageGalleryTableViewController: UITableViewController {
                 if (self.galleryNames.count == 1) {
                     tableView.insertSections([0], with: .automatic)
                 } else if (self.deletedGalleries.count == 0) {
-                    tableView.deleteSections([1], with: .automatic)
+                    tableView.deleteSections([self.galleryNames.isEmpty ? 0 : 1], with: .automatic)
                 }
                 tableView.deleteRows(at: [indexPath], with: .automatic)
                 tableView.insertRows(at: [IndexPath(row: self.galleryNames.count - 1, section: 0)], with: .automatic)
@@ -172,38 +155,5 @@ class ImageGalleryTableViewController: UITableViewController {
 
         actions.append(recoverAction)
         return UISwipeActionsConfiguration(actions: actions)
-    }
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let cell = sender as? ImageGalleryTableViewCell, let rowName = cell.textLabel?.text, let destinationGallery = models[rowName] {
-            if segue.identifier == "Choose Gallery" {
-                if let destination = segue.destination as? ImageGalleryCollectionViewController {
-                    // set the image gallery to be the appropiate array of Image instances.
-                    destination.imageGallery = destinationGallery
-                    // set the title of the segue.destination to be name of the picked gallery.
-                    destination.title = rowName
-                }
-            }
-        }
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    
-    
-    @IBAction func addGallery(_ sender: Any) {
-        guard let galleryName = self.moreGalleryName.popLast() else {
-            print("out of gallery names")
-            return
-        }
-        galleryNames.append(galleryName)
-        tableView.performBatchUpdates({
-            if (galleryNames.count == 1){
-                tableView.insertSections([0], with: .automatic)
-            }
-            tableView.insertRows(at: [IndexPath(row: galleryNames.count - 1, section: 0)], with: .automatic)
-        }, completion: nil)
     }
 }
